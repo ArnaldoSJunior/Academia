900f1ce060cc4e7aaeb50b9f5d3296732372dca6
Arnaldo Junior <arnaldojuniorads@gmail.com>
2024-06-12 20:18:48 -0300

    tela de cadastro andamento

--- Cadastro.php
+++ Cadastro.php
@@ -5,35 +5,49 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
 </head>
 <body style="background-color: #032030;">
-
-<div><form>
-<div class="row">
-     <div class="col">
-         <input type="text" class="form-control" placeholder="Usuario" aria-label="Usuario">
-     </div>
-     <div class="col">
-         <input type="text" class="form-control" placeholder="Nome" aria-label="Nome">
-     </div>
-     <div class="col">
-         <input type="text" class="form-control" placeholder="Sobrenome" aria-label="Sobrenome">
-     </div>
-     <div class="col">
-         <input type="text" class="form-control" placeholder="E-mail" aria-label="E-mail">
-     </div>
-     <div class="col">
-         <input type="text" class="form-control" placeholder="Senha" aria-label="Senha">
-     </div>
-   
-     <input class="btn btn-primary shadow" type="submit" value="Submit" style="background-color: #004D74;">
-</div>
-</form></div>
-
-
-
-
-    
+    <div  class="container-md position-absolute top-50 start-50 translate-middle text-white p-3 shadow-lg rounded" style="background-color: #006DA4;">
+        <h2 style="color: #FFFFFF; text-align: center;">Cadastre-se</h2>
+        <div style="display: flex; justify-content: center;">
+            <form>
+                <div class="row">
+                    <div class="col-md-14 mb-3">
+                        <input type="text" class="form-control" placeholder="Usuario" aria-label="Usuario">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-14 mb-3">
+                        <input type="text" class="form-control" placeholder="Nome" aria-label="Nome">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-14 mb-3">
+                        <input type="text" class="form-control" placeholder="Sobrenome" aria-label="Sobrenome">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-14 mb-3">
+                        <input type="text" class="form-control" placeholder="E-mail" aria-label="E-mail">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-14 mb-3">
+                        <input type="password" class="form-control" placeholder="Telefone" aria-label="Telefone">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-14 mb-3">
+                        <input type="password" class="form-control" placeholder="Senha" aria-label="Senha">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 text-center mt-3">
+                        <input class="btn btn-primary shadow" type="submit" value="Cadastrar" style="background-color: #004D74;">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
